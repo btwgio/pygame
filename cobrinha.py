50e8787662273b496f2fa27e524161954c755b46
Giovanna <giovm14@gmail.com>
2023-12-12 21:46:13 -0300

    testandow

---
+++ cobrinha.py
@@ -0,0 +1,22 @@
+import pygame
+import sys 
+
+pygame.init()
+
+#tamanho da tela
+screen = pygame.display.set_mode((800, 800))
+#nome do jogo
+pygame.display.set_caption('Cobrinha')
+
+cobra = [( r'C:\Users\giov_\Downloads\Programação\pygame\acobra.png')]
+
+#tela preta
+ctela = r'C:\Users\giov_\Downloads\Programação\pygame\fundo.png'
+tela = pygame.image.load(ctela)
+
+while True:
+    for evento in pygame.event.get():
+        #fechar o jogo
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
